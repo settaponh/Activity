9ffd4ace411d5be69d2bca931ab45940f926902e
Settapon Hantacheva <settapon@gmail.com>
2021-12-11 09:28:39 -0800

    Add project files.

---
+++ Activity.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Activity.API.Models;
+
+namespace Activity.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base (options)
+        {
+
+        }
+        public DbSet<Activity.API.Models.Activity> Activities { get; set; }
+    }
+}
